9280b161f35d82007c10a88b6947e70ae10e4996
Robson <robson@gmail.com>
2011-12-14 18:18:06 -0200

    CandlestickFactoryTest

--- src/test/java/br/com/caelum/argentum/CandlestickFactoryTest.java
+++ src/test/java/br/com/caelum/argentum/CandlestickFactoryTest.java
@@ -1,8 +1,34 @@
 package br.com.caelum.argentum;
 
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 
 public class CandlestickFactoryTest {
 
-	
+	@Test
+	public void testSimplesSequenciaDeNegocios(){
+		Calendar hoje = Calendar.getInstance();
+		
+		Negocio negocio1 = new Negocio(40.5, 100, hoje);
+		Negocio negocio2 = new Negocio(45.0, 100, hoje);
+		Negocio negocio3 = new Negocio(39.8, 100, hoje);
+		Negocio negocio4 = new Negocio(42.3, 100, hoje);
+		
+		List<Negocio> negocios = Arrays.asList(negocio1, negocio2, negocio3, negocio4);
+		
+		CandlestickFacktory fabrica = new CandlestickFacktory();
+		Candlestick candle = fabrica.constroiCandlestickParaData(hoje, negocios);
+		
+		Assert.assertEquals(40.5, candle.getAbertura(), 0.00001);
+		Assert.assertEquals(42.3, candle.getFechamento(), 0.00001);
+		Assert.assertEquals(39.8, candle.getMinimo(), 0.00001);
+		Assert.assertEquals(45.0, candle.getMaximo(), 0.00001);
+		Assert.assertEquals(16760.0, candle.getVolume(), 0.00001);
+	}
 	
 }
